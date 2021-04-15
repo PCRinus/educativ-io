6b8a019d2ebb68b228a4e37e7e77788475b18044
Mircea Casapu <mircea.casapu@gmail.com>
2021-04-15 23:20:50 +0300

    added frontedn routers and a few demo routes

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,1 +1,14 @@
-<h1>Hello!</h1>
+<script>
+    import Router from "svelte-spa-router";
+    import Home from "./Pages/Home.svelte";
+    import Dashboard from "./Pages/Dashboard.svelte";
+    import Profile from "./Pages/Profile.svelte"
+
+    const routes = {
+        "/": Home,
+        "/dashboard": Dashboard,
+        "/profile": Profile,
+    }
+</script>
+
+<Router {routes}/>
