0a3c14112d7ea5764cfe8cfcf936830db7c17809
Mircea Casapu <mircea.casapu@gmail.com>
2021-04-18 23:20:15 +0300

    aded login consumer for api endpoint some views

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -3,11 +3,13 @@
     import Home from "./Pages/Home.svelte";
     import Dashboard from "./Pages/Dashboard.svelte";
     import Profile from "./Pages/Profile.svelte"
+    import Signup from "./Pages/Signup.svelte";
 
     const routes = {
         "/": Home,
         "/dashboard": Dashboard,
         "/profile": Profile,
+        "/signup": Signup
     }
 </script>
 
--- client/src/Pages/Home.svelte
+++ client/src/Pages/Home.svelte
@@ -1,1 +1,8 @@
-<h1>Home</h1>
+<section class="hero is-link is-fullheight-with-navbar">
+    <div class="hero-body">
+        <div class="container">
+            <h2>Educativ.io</h2>
+            <a href="#/signup" class="button is-primary is-light">Sign up</a>
+        </div>
+    </div>
+</section>
