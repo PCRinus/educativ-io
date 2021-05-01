6fa6909c7b59c05c26a624779127e672d00487a6
Mircea Casapu <mircea.casapu@gmail.com>
2021-05-01 23:30:05 +0300

    fixing my shit

--- client/src/pages/Home.svelte
+++ client/src/pages/Home.svelte
@@ -2,7 +2,7 @@
     <div class="hero-body">
         <div class="container">
             <h2>Educativ.io</h2>
-            <a href="#/signup" class="button is-primary is-light">Sign up</a>
+            <a href="#/signup" class="button is-primary is-light">Sign up TEST</a>
         </div>
     </div>
 </section>
