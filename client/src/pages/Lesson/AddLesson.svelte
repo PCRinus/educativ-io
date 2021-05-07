73374c5634b8f808afdcd7adf9e0e5f0c1d1f8de
Mircea Casapu <mircea.casapu@gmail.com>
2021-05-07 23:39:36 +0300

    preliminary changes to markdown field in add lesson page

--- client/src/pages/Lesson/AddLesson.svelte
+++ client/src/pages/Lesson/AddLesson.svelte
@@ -3,20 +3,48 @@
   import { lessonsData } from "../../stores";
   import { push } from "svelte-spa-router";
   import slugify from "slugify";
+  import marked from "marked";
   import PageTransitions from "../../components/PageTransitions.svelte";
 
   let lessonTitle = "";
   let lessonDescription = "";
   let lessonParentCategory = "";
-  let markdown = "";
+  // let lessonMarkdown = "";
   let slug = "";
+
+  let lessonMarkdown = `
+# H1 heading
+
+## H2 heading
+
+### H3 heading
+
+--------
+
+**bold text**
+
+*italicized text*
+
+--------
+
+1. First item
+2. Second item
+3. Third item
+
+- First item
+- Second item
+- Third item
+
+[Svelte](https://svelte.dev/)
+`;
+  $: markdown = marked(lessonMarkdown);
 
   async function newLesson() {
     const newLesson = {
       title: lessonTitle,
       description: lessonDescription,
       parentCategory: lessonParentCategory,
-      markdown: markdown,
+      markdown: lessonMarkdown,
     };
     const response = await axios.post("/api/lesson", newLesson);
     $lessonsData = [...$lessonsData, response.data];
@@ -63,10 +91,21 @@
         />
       </div>
 
-      <div class="field">
+      <!-- <div class="field">
         <label for="" class="label">Markdown</label>
-        <input type="text" class="input" bind:value={markdown} required />
-      </div>
+        <input type="text" class="input" bind:value={lessonMarkdown} required />
+      </div> -->
+
+      <label for="" class="label">Markdown</label>
+      <div class="markdown-editor">
+        <div class="left-panel">
+            <textarea bind:value={lessonMarkdown} class="source"></textarea>
+        </div>
+    
+        <div class="right-panel">
+            <div class="output">{@html markdown}</div>
+        </div>
+    </div>
 
       <div class="control">
         <input type="submit" class="button is-link" value="Add lesson" />
@@ -74,3 +113,37 @@
     </form>
   </div>
 </PageTransitions>
+
+<style>
+    .markdown-editor {
+        width: 100%;
+        display: flex;
+        align-items:flex-start;
+        justify-content: space-evenly;
+    }
+    .left-panel, .right-panel {
+        width: 50%;
+        border: solid 1px;
+        border-color: #b5b5b5;
+        border-radius: 4px;
+        height: 85vh;
+        background: #ffffff;
+    }
+    .right-panel {
+        overflow: auto;
+    }
+    .source {
+        border: none;
+        width: 100%;
+        height: 100%;
+        background: #2a2e41;
+        color: #ffffff;
+    }
+    .source:focus {
+        outline: none;
+    }
+    .output {
+        width: 100%;
+        padding: 0 2em;
+    }
+</style>
--- client/src/pages/Lesson/ListSpecificLesson.svelte
+++ client/src/pages/Lesson/ListSpecificLesson.svelte
@@ -2,7 +2,10 @@
   import axios from "axios";
   import { onMount } from "svelte";
   import { currentLessonData, selectedLessonSlug } from "../../stores";
+  import marked from "marked";
   import PageTransitions from "../../components/PageTransitions.svelte";
+
+  $: markdown = marked($currentLessonData.markdown);
 
   onMount(async () => {
     const { data } = await axios.get("/api/lesson/" + $selectedLessonSlug);
@@ -15,6 +18,6 @@
     <h1>{$currentLessonData.title}</h1>
     <h4>{$currentLessonData.description}</h4>
     <h4>{$currentLessonData.createdAt}</h4>
-    <p>{$currentLessonData.markdown}</p>
+    <p>{@html markdown}</p>
   </div>
 </PageTransitions>
