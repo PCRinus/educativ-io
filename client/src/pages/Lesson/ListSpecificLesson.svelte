0b0d91ba75c8102a97c5760d088d35fc4f7f9722
Mircea Casapu <mircea.casapu@gmail.com>
2021-05-08 16:14:56 +0300

    added await block to lis specific lesson

--- client/src/pages/Lesson/ListSpecificLesson.svelte
+++ client/src/pages/Lesson/ListSpecificLesson.svelte
@@ -8,13 +8,19 @@
     const { data } = await axios.get("/api/lesson/" + $selectedLessonSlug);
     $currentLessonData = data;
   });
+
+  let asdf = $currentLessonData;
+
+  let promise = onMount();
 </script>
 
 <PageTransitions>
   <div class="container">
-    <h1>{$currentLessonData.title}</h1>
-    <h4>{$currentLessonData.description}</h4>
-    <h4>{$currentLessonData.createdAt}</h4>
-    <p>{$currentLessonData.markdown}</p>
+    {#await promise then asdf}
+      <h1>{$currentLessonData.title}</h1>
+      <h4>{$currentLessonData.description}</h4>
+      <h4>{$currentLessonData.createdAt}</h4>
+      <p>{$currentLessonData.markdown}</p>
+    {/await}
   </div>
 </PageTransitions>
