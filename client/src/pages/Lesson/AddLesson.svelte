285ff70ed9bc5ab9d6dbe29e049044c4b1484c7a
Mircea Casapu <mircea.casapu@gmail.com>
2021-05-08 23:21:41 +0300

    some improvements to addin lesson flow

--- client/src/pages/Lesson/AddLesson.svelte
+++ client/src/pages/Lesson/AddLesson.svelte
@@ -1,73 +1,39 @@
 <script>
+  import {
+    currentLessonTitle,
+    currentLessonDescription,
+    currentLessonCategory,
+    categoriesData,
+  } from "../../stores";
+  import { onMount } from "svelte";
   import axios from "axios";
-  import { lessonsData } from "../../stores";
   import { push } from "svelte-spa-router";
-  import slugify from "slugify";
-  import marked from "marked";
   import PageTransitions from "../../components/PageTransitions.svelte";
 
-  let lessonTitle = "";
-  let lessonDescription = "";
-  let lessonParentCategory = "";
-  let slug = "";
+  onMount(async () => {
+    const { data } = await axios.get("/api/categories");
+    $categoriesData = data;
+  });
 
-  let lessonMarkdown = `
-# H1 heading
-
-## H2 heading
-
-### H3 heading
-
---------
-
-**bold text**
-
-*italicized text*
-
---------
-
-1. First item
-2. Second item
-3. Third item
-
-- First item
-- Second item
-- Third item
-
-[Svelte](https://svelte.dev/)
-`;
-  $: markdown = marked(lessonMarkdown);
-
-  async function newLesson() {
-    const newLesson = {
-      title: lessonTitle,
-      description: lessonDescription,
-      parentCategory: lessonParentCategory,
-      markdown: lessonMarkdown,
-    };
-    const response = await axios.post("/api/lesson", newLesson);
-    $lessonsData = [...$lessonsData, response.data];
-    // slug = slugify(response.data.slug);
-    // redirectToLesson(slug);
-    redirectToDashboard();
+  function redirectToEditor() {
+    push("/new-lesson/markdown");
   }
 
-  function redirectToLesson(slug) {
-    push("/lessons/" + slug);
-  }
-
-  function redirectToDashboard() {
-    push("/dashboard");
-  }
+  let selected;
 </script>
 
 <PageTransitions>
   <div class="container">
-    <h1>New Lesson</h1>
-    <form on:submit|preventDefault={newLesson}>
+    <h1>Add lesson details</h1>
+    <form on:submit|preventDefault={redirectToEditor}>
       <div class="field">
         <label for="" class="label">Lesson Title</label>
-        <input type="text" class="input" bind:value={lessonTitle} required />
+        <input
+          type="text"
+          class="input"
+          bind:value={$currentLessonTitle}
+          required
+        />
       </div>
 
       <div class="field">
@@ -75,70 +41,59 @@
         <input
           type="text"
           class="input"
-          bind:value={lessonDescription}
+          bind:value={$currentLessonDescription}
           required
         />
       </div>
 
       <div class="field">
         <label for="" class="label">Category</label>
-        <input
-          type="text"
-          class="input"
-          bind:value={lessonParentCategory}
-          required
-        />
-      </div>
-
-      <label for="" class="label">Markdown</label>
-      <div class="markdown-editor">
-        <div class="left-panel">
-          <textarea bind:value={lessonMarkdown} class="source" />
-        </div>
-
-        <div class="right-panel">
-          <div class="output">{@html markdown}</div>
-        </div>
+        <!-- svelte-ignore a11y-no-onchange -->
+        <select
+          class="category-selector"
+          bind:value={$currentLessonCategory}
+        >
+        <option disabled selected>Please choose a category</option>
+          {#each $categoriesData as category}
+            <option value={category}>
+              {category.name}
+            </option>
+          {/each}
+        </select>
       </div>
 
       <div class="control">
-        <input type="submit" class="button is-link" value="Add lesson" />
+        <input
+          type="submit"
+          class="button is-link go-to-editor"
+          value="Go to editor > "
+        />
       </div>
     </form>
   </div>
 </PageTransitions>
 
 <style>
-  .markdown-editor {
-    width: 100%;
-    display: flex;
-    align-items: flex-start;
-    justify-content: space-evenly;
+  .control {
+    display: flex !important;
+    flex-direction: row-reverse;
   }
-  .left-panel,
-  .right-panel {
-    width: 50%;
-    border: solid 1px;
-    border-color: #b5b5b5;
+  .category-selector {
+    box-shadow: inset 0 0.0625em 0.125em rgba(10, 10, 10, 0.05);
+    max-width: 100%;
+    width: 100%;
+    background-color: #fff;
+    border-color: #dbdbdb;
     border-radius: 4px;
-    height: 85vh;
-    background: #ffffff;
+    color: #363636;
+    height: 2.5em;
+    font-size: 1rem;
+    padding-bottom: calc(0.5em - 1px);
+    padding-left: calc(0.75em - 1px);
+    padding-right: calc(0.75em - 1px);
+    padding-top: calc(0.5em - 1px);
   }
-  .right-panel {
-    overflow: auto;
-  }
-  .source {
-    border: none;
-    width: 100%;
-    height: 100%;
-    background: #2a2e41;
-    color: #ffffff;
-  }
-  .source:focus {
-    outline: none;
-  }
-  .output {
-    width: 100%;
-    padding: 0 2em;
+  .category-selector:hover {
+    border-color: #b5b5b5;
   }
 </style>
---
+++ client/src/pages/Lesson/AddLessonMarkdown.svelte
@@ -0,0 +1,26 @@
+<script>
+  import {
+    lessonsData,
+    currentLessonTitle,
+    currentLessonDescription,
+    currentLessonCategory,
+    currentLessonMarkdown,
+  } from "../../stores";
+  import { onMount, onDestroy } from "svelte";
+  import PageTransitions from "../../components/PageTransitions.svelte";
+
+  onDestroy(()=> {
+    $currentLessonTitle = '';
+    $currentLessonDescription = '';
+    $currentLessonCategory = [];
+  })
+</script>
+
+<PageTransitions>
+  <div class="container">
+    <h1>Markdown Editor</h1>
+    <p>{$currentLessonTitle}</p>
+    <p>{$currentLessonDescription}</p>
+    <p>{$currentLessonCategory.name}</p>
+  </div>
+</PageTransitions>
