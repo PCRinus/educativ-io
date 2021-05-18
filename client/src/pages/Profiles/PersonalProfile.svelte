a696f283f18afdd41e338bdd4490b3c4cc331982
Mircea Casapu <mircea.casapu@gmail.com>
2021-05-18 16:01:33 +0300

    removed more code that was unnecesarry

--- client/src/pages/Profiles/PersonalProfile.svelte
+++ client/src/pages/Profiles/PersonalProfile.svelte
@@ -7,11 +7,6 @@
   import LessonList from "../../components/LessonList.svelte";
   import PageTransitions from "../../components/PageTransitions.svelte";
 
-  let hideProfileUpdateContainer = true;
-  let firstName = "";
-  let lastName = "";
-  let age = 0;
-
   onMount(async () => {
     await axios
       .all([
@@ -25,17 +20,6 @@
         })
       );
   });
-
-  async function setProfileChanges() {
-    const profile = {
-      firstName: firstName,
-      lastName: lastName,
-      age: age,
-    };
-    const response = await axios.post("/api/profile", profile);
-    $userProfile = [response.data];
-    hideProfileUpdateContainer = !hideProfileUpdateContainer;
-  }
 </script>
 
 <PageTransitions>
@@ -50,41 +34,5 @@
         <LessonList lessonList={$lessonsData} />
       </div>
     </div>
-    <div class:show-profile-container={hideProfileUpdateContainer}>
-      <h1>Update your profile</h1>
-
-      <form on:submit|preventDefault={setProfileChanges}>
-        <div class="field">
-          <label for="" class="label">First Name</label>
-          <input type="text" class="input" bind:value={firstName} required />
-        </div>
-
-        <div class="field">
-          <label for="" class="label">Last Name</label>
-          <input type="text" class="input" bind:value={lastName} required />
-        </div>
-
-        <div class="field">
-          <label for="" class="label">Age Name</label>
-          <input
-            type="number"
-            class="input"
-            min="14"
-            bind:value={age}
-            required
-          />
-        </div>
-
-        <div class="control">
-          <input type="submit" class="button is-link" value="Submit" />
-        </div>
-      </form>
-    </div>
   </div>
 </PageTransitions>
-
-<style>
-  .show-profile-container {
-    display: none;
-  }
-</style>
