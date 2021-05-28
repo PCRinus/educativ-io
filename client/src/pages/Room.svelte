2238f76d3635e9639c5bf38af7e41a28c146d04c
Mircea Casapu <mircea.casapu@gmail.com>
2021-05-28 21:54:41 +0300

    some videochat functionality

--- client/src/pages/Room.svelte
+++ client/src/pages/Room.svelte
@@ -1,16 +1,28 @@
 <script>
-  import { uuidKey, uuidRedirectButton} from "../stores";
+  import { uuidKey, uuidRedirectButton } from "../stores";
   import { onDestroy } from "svelte";
   import PageTransitions from "../components/PageTransitions.svelte";
 
   onDestroy(() => {
     $uuidKey = "";
     $uuidRedirectButton = false;
-  })
+  });
 </script>
 
 <PageTransitions>
   <div class="container container-custom">
     <h1>Room {$uuidKey}</h1>
-  </div>
+    <!-- <div id="otEmbedContainer" style="width:800px; height:640px" />
+    <script
+      src="https://tokbox.com/embed/embed/ot-embed.js?embedId=3960683f-27f3-47ca-8d12-2f5ee0cb181d&room={$uuidKey}%22%3E"></script>
+  </div> -->
+
+  <iframe
+        src="https://tokbox.com/embed/embed/ot-embed.js?embedId=3960683f-27f3-47ca-8d12-2f5ee0cb181d&room={$uuidKey}&iframe=true"
+        width="800px"
+        height="640px"
+        scrolling="auto"
+        allow="microphone; camera"
+        title="video-chat"
+      ></iframe>
 </PageTransitions>
