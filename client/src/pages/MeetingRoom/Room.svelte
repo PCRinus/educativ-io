16b5bf6b0cf4cfb2c6ab6f6fab658e0d632c756c
Mircea Casapu <mircea.casapu@gmail.com>
2021-06-02 23:34:07 +0300

    added new urls for videochat

--- client/src/pages/MeetingRoom/Room.svelte
+++ client/src/pages/MeetingRoom/Room.svelte
@@ -5,9 +5,9 @@
   import PageTransitions from "../../components/PageTransitions.svelte";
 
   let localUrl =
-    "https://tokbox.com/embed/embed/ot-embed.js?embedId=3960683f-27f3-47ca-8d12-2f5ee0cb181d&room=DEFAULT_ROOM&iframe=true";
+    "https://tokbox.com/embed/embed/ot-embed.js?embedId=ca26ca5b-c90e-42bb-8559-6436b932993a&room=DEFAULT_ROOM&iframe=true";
   let serverUrl =
-    "https://tokbox.com/embed/embed/ot-embed.js?embedId=dc381aeb-37e5-43c2-ba95-495863cf8fad&room=DEFAULT_ROOM&iframe=true";
+    "https://tokbox.com/embed/embed/ot-embed.js?embedId=8bbd5dc5-e360-40bc-afef-702113afdc1c&room=DEFAULT_ROOM&iframe=true";
 
   onDestroy(() => {
     $uuidKey = "";
