2d3bdecbaf0d999e9a3c47bdff8ae28e3022910c
Mircea Casapu <mircea.casapu@gmail.com>
2021-06-02 22:23:30 +0300

    cleaned up meeting room markup

--- client/src/pages/MeetingRoom/Room.svelte
+++ client/src/pages/MeetingRoom/Room.svelte
@@ -17,23 +17,10 @@
 
 <PageTransitions>
   <div class="container container-custom">
-    <h1>Room {$uuidKey}</h1>
-    <!-- <div id="otEmbedContainer" style="width:800px; height:640px" />
-    <script
-      src="https://tokbox.com/embed/embed/ot-embed.js?embedId=3960683f-27f3-47ca-8d12-2f5ee0cb181d&room={$uuidKey}%22%3E"></script>
-  </div> -->
     {#if process.env.NODE_ENV === "production"}
       <VideoChat videoUrl={serverUrl} />
     {:else}
       <VideoChat videoUrl={localUrl} />
     {/if}
-    <!-- <iframe
-      src="https://tokbox.com/embed/embed/ot-embed.js?embedId=dc381aeb-37e5-43c2-ba95-495863cf8fad&room={$uuidKey}&iframe=true"
-      width="800px"
-      height="640px"
-      scrolling="auto"
-      allow="microphone; camera"
-      title="video-chat"
-    /> -->
   </div></PageTransitions
 >
